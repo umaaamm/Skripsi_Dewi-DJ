3bf531019d73b661e5f74d26297665c4c6db3ee4
umaaamm <umam.tekno@gmail.com>
2018-07-15 16:30:49 +0700

    add detail produk

--- app/src/main/java/com/example/umaaamm/dewi_jilbab/MainActivity.java
+++ app/src/main/java/com/example/umaaamm/dewi_jilbab/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.umaaamm.dewi_jilbab;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,8 +20,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> idbarang;
+    private ArrayList<String> stokbarang;
+    private ArrayList<String> namabarang;
+    private ArrayList<String> gambarbarang;
+
+    private String JSON_STRING;
+    SwipeRefreshLayout swipeRefreshLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +48,37 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //menu
-        //DrawerLayout perkategori = (DrawerLayout) findViewById(R.id.perkategori);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary, R.color.colorPrimary);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                        startActivity(getIntent());
+                        swipeRefreshLayout.setRefreshing(false);
+
+                    }
+                }, 2000);
+            }
+        });
+
+
+        idbarang = new ArrayList<>();
+        stokbarang = new ArrayList<>();
+        namabarang = new ArrayList<>();
+        gambarbarang = new ArrayList<>();
+
+        rvView = (RecyclerView) findViewById(R.id.rv_main_barang);
+        rvView.setHasFixedSize(true);
+
+        rvView.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
+
+        getJSON();
+
+
 
         //floating button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -49,17 +102,96 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
-        //button menu
-//       perkategori.setOnClickListener(new View.OnClickListener() {
-//           @Override
-//           public void onClick(View v) {
-//               Toast.makeText(MainActivity.this,
-//                       "Your Message", Toast.LENGTH_LONG).show();
-//           }
-//       });
-
 
     }
+
+    private void showEmployee(){
+
+        if(!stokbarang.isEmpty()){
+            stokbarang.clear();
+        }
+
+        if(!namabarang.isEmpty()){
+            namabarang.clear();
+        }
+
+        if(!gambarbarang.isEmpty()){
+            gambarbarang.clear();
+        }
+
+        JSONObject jsonObject = null;
+//        ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String, String>>();
+        try {
+            jsonObject = new JSONObject(JSON_STRING);
+            JSONArray result = jsonObject.getJSONArray(KonfigurasiBarang.TAG_JSON_ARRAY);
+
+            for(int i = 0; i<result.length(); i++){
+                JSONObject jo = result.getJSONObject(i);
+                String id = jo.getString(KonfigurasiBarang.TAG_ID_BARANG);
+                String stok = jo.getString(KonfigurasiBarang.TAG_STOK);
+                String nama = jo.getString(KonfigurasiBarang.TAG_NAMA);
+                String gambar = jo.getString(KonfigurasiBarang.TAG_GAMBAR);
+
+                //Toast.makeText(Barang.this,"Get Json : "+nama,Toast.LENGTH_SHORT).show();
+                idbarang.add(id);
+                stokbarang.add(stok);
+                namabarang.add(nama);
+                gambarbarang.add(gambar);
+
+//                HashMap<String,String> employees = new HashMap<>();
+//                employees.put(Konfigurasi.TAG_ID,id);
+//                employees.put(Konfigurasi.TAG_NAMA,nama_kategor);
+//                list.add(employees);
+            }
+
+            //Toast.makeText(Barang.this,"Get Json : "+namabarang.size(),Toast.LENGTH_SHORT).show();
+            adapter = new RecyclerViewAdapterBarang(idbarang,stokbarang,namabarang,gambarbarang);
+            rvView.setAdapter(adapter);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+//        ListAdapter adapter = new SimpleAdapter(
+//                TampilSemuaPgw.this, list, R.layout.list_item,
+//                new String[]{konfigurasi.TAG_ID,konfigurasi.TAG_NAMA},
+//                new int[]{R.id.id, R.id.name});
+//
+//        listView.setAdapter(adapter);
+    }
+
+    private void getJSON(){
+
+        class GetJSON extends AsyncTask<Void,Void,String> {
+
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this,"Mengambil Data","Mohon Tunggu...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                JSON_STRING = s;
+                showEmployee();
+
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                RequestHandler rh = new RequestHandler();
+                String s = rh.sendGetRequest(KonfigurasiBarang.URL_GET_ALL);
+                return s;
+            }
+        }
+        GetJSON gj = new GetJSON();
+        gj.execute();
+    }
+
+
 
     @Override
     public void onBackPressed() {
