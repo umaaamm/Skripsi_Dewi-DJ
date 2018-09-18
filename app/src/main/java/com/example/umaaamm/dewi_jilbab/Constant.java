0f593a5d77b48009b4793d03ba29aacabc7e8f12
umaaamm <umam.tekno@gmail.com>
2018-09-18 17:13:44 +0700

    udah bisa input rating

--- app/src/main/java/com/example/umaaamm/dewi_jilbab/Constant.java
+++ app/src/main/java/com/example/umaaamm/dewi_jilbab/Constant.java
@@ -6,5 +6,6 @@ package com.example.umaaamm.dewi_jilbab;
 
 public class Constant {
     public static final String YOUTUBE_API_KEY = "AIzaSyCLTghYo1hszuaHfyFQPLeHB2PTGc6T7ag";
-    public static final String YOUTUBE_ID = "Ec_42Y_322g";
+    //public static final String YOUTUBE_ID = "Ec_42Y_322g";
+    public static final String YOUTUBE_ID = "k8Zbb2vSXQQ";
 }
